c451fd2ce751673fb0879298d88eac7c31232f38
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-23 17:07:32 +0530

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into ALIEN values (1,'anirudh','java');
+insert into ALIEN values (2,'avinash','python');
+insert into ALIEN values (3,'ravi','android');
+insert into ALIEN values (4,'pavan','go');
+insert into ALIEN values (5,'anish','react');
+insert into ALIEN values (6,'mohan','java');
+insert into ALIEN values (7,'pranav','java');
+insert into ALIEN values (8,'pravin','java');
