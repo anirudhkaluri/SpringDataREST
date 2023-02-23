779fc5abe5ec075e26a9b784b8086f34f97cb237
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-23 17:40:25 +0530

    AlienRepo uses Rest services of spring. so no need to write controllers

--- src/main/java/com/SpringDataRest/demo/AlienRepo.java
+++ src/main/java/com/SpringDataRest/demo/AlienRepo.java
@@ -1,9 +1,13 @@
 package com.SpringDataRest.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.SpringDataRest.demo.model.Alien;
 
+
+//collectionResourceRel specifies base url we use //path is aliens
+@RepositoryRestResource(collectionResourceRel="aliens",path="aliens")
 public interface AlienRepo extends JpaRepository <Alien,Integer>{
 
 }
